6aaecbbde3b1de555f0866ca4e58413f3b1f2d29
fsayik <f.sayik00@gmail.com>
2025-10-07 13:41:55 +0300

    Initial Commit

---
+++ NewScope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewScope
+//
+//  Created by Ferhat Şayık on 7.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
